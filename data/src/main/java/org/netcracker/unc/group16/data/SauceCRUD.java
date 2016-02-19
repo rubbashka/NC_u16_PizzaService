e43e29f34a1823f96ce421f3aa5e53d3fc540db4
rubbashka <rubbashka@gmail.com>
2016-02-19 22:01:26 +0400

    Реализована вставка в SAUCES

--- data/src/main/java/org/netcracker/unc/group16/data/SauceCRUD.java
+++ data/src/main/java/org/netcracker/unc/group16/data/SauceCRUD.java
@@ -3,26 +3,25 @@ package org.netcracker.unc.group16.data;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 
-
-public class PizzasCRUD {
+public class SauceCRUD {
 
     //Create
-    public void insertIntoPizzas(){
+    public void insertIntoSauce(){
         JDBC jdbc = new JDBC();
         Connection conn = jdbc.setConnection();
 
 
-        String sql = "INSERT INTO PIZZAS (pz_id, def, comments) VALUES (?, ?, ?)";
+        String sql = "INSERT INTO SAUCES (sc_id, def, comments) VALUES (?, ?, ?)";
 
         try{
             PreparedStatement statement = conn.prepareStatement(sql);
             statement.setNull(1, java.sql.Types.VARCHAR);
-            statement.setString(2, "Mocarella");
-            statement.setString(3, "Classical Itallian pizza");
+            statement.setString(2, "Томаты");
+            statement.setNull(3, java.sql.Types.VARCHAR);
 
             int rowsInserted = statement.executeUpdate();
             if (rowsInserted > 0) {
-                System.out.println("Inserted into PIZZAS successfully.");
+                System.out.println("Inserted into SAUCES successfully.");
             }
         }
         catch (java.sql.SQLException e){
--- view/src/main/java/org/netcracker/unc/group16/view/PizzaService.java
+++ view/src/main/java/org/netcracker/unc/group16/view/PizzaService.java
@@ -2,14 +2,17 @@ package org.netcracker.unc.group16.view;
 
 import org.netcracker.unc.group16.data.JDBC;
 import org.netcracker.unc.group16.data.PizzasCRUD;
+import org.netcracker.unc.group16.data.SauceCRUD;
 
 /**
  * Created by Ivan on 18.02.2016.
  */
 public class PizzaService {
     public static void main (String args[]){
-        PizzasCRUD pizzasCRUD = new PizzasCRUD();
-        pizzasCRUD.insertIntoPizzas();
+      //  PizzasCRUD pizzasCRUD = new PizzasCRUD();
+       // pizzasCRUD.insertIntoPizzas();
 
+        SauceCRUD sauceCRUD = new SauceCRUD();
+        sauceCRUD.insertIntoSauce();
     }
 }
