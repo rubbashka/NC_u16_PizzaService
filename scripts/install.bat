827764f4647b8ca7199e9fba7277db7dd8afcf70
rubbashka <rubbashka@gmail.com>
2016-02-28 20:49:17 +0400

    -

--- scripts/install.bat
+++ scripts/install.bat
@@ -2,4 +2,4 @@ rem Server encoding: AMERICAN_AMERICA.AL32UTF8
 rem IDE file encoding: UTF-8
 set NLS_LANG=AMERICAN_AMERICA.AL32UTF8
 chcp 1251
-sqlplus IBR_TEST/employer@smrsupp @setup.sql
+sqlplus PIZZADB/PIZZADB@XE @setup.sql
