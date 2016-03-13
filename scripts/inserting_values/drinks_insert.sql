90ed88c6feb7864c8630421945cd80c698889a41
Ivan <rubbashka@gmail.com>
2016-03-13 16:21:10 +0400

    Изменены названия скриптов

--- scripts/inserting_values/drinks_insert.sql
+++ scripts/inserting_values/drinks_insert.sql
@@ -1,4 +1,4 @@
 PROMPT drinks_insert...
-INSERT INTO DRINKS (drnk_id, volume, price, article, description) VALUES (null, 200 , 20, 'Черный чай', 'С бергамотом');
-INSERT INTO DRINKS (drnk_id, volume, price, article, description) VALUES (null, 500 , 100, 'Молочный коктейль', null);
+INSERT INTO DRINKS (drnk_id, volume, price, title, description) VALUES (null, 200 , 20, 'Черный чай', 'С бергамотом');
+INSERT INTO DRINKS (drnk_id, volume, price, title, description) VALUES (null, 500 , 100, 'Молочный коктейль', null);
 commit;
